fc41d28cfee7268a4cd9b97ba293344d04ce24da
Anand032 <andrew.maqwan@gmail.com>
2018-07-11 20:17:27 +0530

    just removed unused

--- app/src/main/java/com/app/android/cync/LoginActivity.java
+++ app/src/main/java/com/app/android/cync/LoginActivity.java
@@ -587,8 +587,6 @@ public class LoginActivity extends Activity {
         }
     }
 
-    //------------------------
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         //callbackManager.onActivityResult(requestCode, resultCode, data);
